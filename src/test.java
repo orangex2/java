9be2bfd333593d2db8e34e93ddd76fd4e0a6ee2e
orangex2 <mzc02-leehandu@MZC01-LEEHANDU.local>
2020-11-10 15:53:45 +0900

    upate 001

--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) throws Exception {
         System.out.println("\n*******************************\n");
         System.out.println("\n++++++++++++++++++++++++++++++++\n");
+        System.out.println("\n hi ~~~ hi ~~~~n");
         //test
     }
 }
