0c0970f8e762070e8bd8d3de83b583fe5a376487
orangex2 <mzc02-leehandu@MZC01-LEEHANDU.local>
2020-11-10 15:15:49 +0900

    java-01

---
+++ src/hans.java
@@ -0,0 +1,6 @@
+public class hans {    
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nhhhhhhhhhhhhhhhhn");
+ 
+    }
+}
--- src/test.java
+++ src/test.java
@@ -1,3 +1,6 @@
 public class test {
-    
+    public static void main(String[] args) throws Exception {
+        System.out.println("\n*******************************\n");
+        //test
+    }
 }
